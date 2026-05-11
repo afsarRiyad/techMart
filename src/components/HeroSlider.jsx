ed68daf30c7d73323518af5b50d9323af24433bd
Nurul Afsar Riyad <nurulafsarriyad06@gmail.com>
2026-05-11 21:25:12 +0600

    fix slider mobile-view responsiveness

--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -16,7 +16,7 @@ const HeroSlider = () => {
     const [activeIndex, setActiveIndex] = useState(0);
   return (
     <>
- <div style={{backgroundImage: `url(${activeIndex === 1 ? ProductBackground : background})`}} className='relative w-full h-[170px] sm:h-[410px] bg-gray-100/30 bg-center transition-all duration-500  bg-cover overflow-hidden'>
+ <div style={{backgroundImage: `url(${activeIndex === 1 ? ProductBackground : background})`}} className='relative w-full h-[220px] sm:h-[410px] bg-gray-100/30 bg-center transition-all duration-500  bg-cover overflow-hidden'>
     <Container>
         <Swiper 
         slidesPerView={1}
@@ -35,14 +35,14 @@ const HeroSlider = () => {
        >
     <SwiperSlide >
       <Container >
-           <div className='flex font-roboto items-center  lg:h-[410px] h-[170px] w-full overflow-hidden'>
+           <div className='flex font-roboto items-center  lg:h-[410px] h-[220px] w-full overflow-hidden'>
               <div className='flex flex-col lg:w-[445px] w-[60%]'>
-                     <span className='text-[#34BCEC] lg:text-base  text-[10px] sm:font-semibold uppercase lg:pb-3 pb-1 tracking-widest anim-topTxt'>
+                     <span className='text-[#34BCEC] lg:text-base  text-[16px] sm:font-semibold uppercase lg:pb-3 pb-1 tracking-widest anim-topTxt'>
                       shop to get what you love
                  </span>
-                 <span className='uppercase w-42 lg:w-110 lg:text-[46px] text-[14px] font-thin text-[#333E48] anim-text lg:leading-14 '>timepieces that make a statement up to <span className='font-inter font-bold'>40% off</span></span>
+                 <span className='uppercase w-42 lg:w-110 lg:text-[46px] text-[14px] lg:font-thin text-[#333E48] anim-text lg:leading-14 leading-6'>timepieces that make a statement up to <span className='font-inter font-bold'>40% off</span></span>
                  <div className='lg:pt-9 pt-2 anim-btn'>
-                     <button className='relative group bg-primary lg:py-3 lg:px-14 px-4 py-1.5 lg:rounded-xl rounded-md tracking-wider lg:text-[16px] text-sm font-thin cursor-pointer overflow-hidden'>
+                     <button className='relative group bg-primary lg:py-3 lg:px-14 px-4 py-1.5 lg:rounded-xl rounded-md tracking-wider font-semibold lg:text-[16px] text-sm lg:font-thin cursor-pointer overflow-hidden'>
                         Start Buying
                     <span className='btnHover'/>
                      </button>
@@ -58,26 +58,26 @@ const HeroSlider = () => {
 
     <SwiperSlide >
       <Container>
-           <div className='flex font-roboto items-center  lg:h-[410px] h-[170px] justify-between w-full overflow-hidden'>
+           <div className='flex font-roboto items-center  lg:h-[410px] h-[220px] justify-between w-full overflow-hidden'>
               <div className='flex flex-col lg:w-[445px] w-[60%]'>
-                 <span className='text-[#34BCEC] lg:text-base whitespace-nowrap text-[12px] sm:font-semibold uppercase lg:pb-3 pb-1 tracking-widest anim-topTxt'>the new stardard </span>
-                     <span className='uppercase w-36 lg:w-110 lg:text-[46px] text-[14px] font-thin text-[#333E48] lg:leading-14 anim-Watch-txt'>
+                 <span className='text-[#34BCEC] lg:text-base whitespace-nowrap text-[16px] sm:font-semibold uppercase lg:pb-3 pb-1 tracking-widest anim-topTxt'>the new stardard </span>
+                     <span className='uppercase w-36 lg:w-110 lg:text-[46px] text-[14px] lg:font-thin text-[#333E48] lg:leading-14 leading-6 anim-Watch-txt'>
                       shop to get what you love
                  </span>
-                 <div className='priceTagAnim'>
+                 <div className='priceTagAnim py-1 lg:py-0'>
                     <span className='align-top lg:text-[40px] text-[12px] lg:font-bold font-semibold'>$</span>
                     <span className='lg:text-[54px] text-[16px] font-bold'>749</span>
                     <span className='align-top lg:text-[40px] text-[12px] lg:font-bold font-semibold'>99</span>
                  </div>
                  <div className='lg:pt-9 pt-2 anim-btn'>
-                     <button className='relative group bg-primary lg:py-3 lg:px-14 px-4 py-1.5 lg:rounded-xl rounded-md tracking-wider lg:text-[16px] text-sm font-thin cursor-pointer overflow-hidden'>
+                     <button className='relative group bg-primary lg:py-3 font-semibold lg:px-14 px-4 py-1.5 lg:rounded-xl rounded-md tracking-wider lg:text-[16px] text-sm lg:font-thin cursor-pointer overflow-hidden'>
                         Start Buying
                     <span className='btnHover'/>
                      </button>
                  </div>
               </div >
               <div className='flex-1 flex justify-center lg:pl-40'>
-                <img src={watchess} className='lg:h-[790px] h-[400px] lg:pt-30 pl-1 lg:pl-0 w-auto object-contain anim-Watch-img' alt="watches" />
+                <img src={watchess} className='lg:h-[790px] h-auto lg:pt-30 pl-1 lg:pl-0 w-auto  anim-Watch-img' alt="watches" />
               </div>
            </div>
       </Container>
@@ -85,21 +85,21 @@ const HeroSlider = () => {
  
     <SwiperSlide >
       <Container>
-           <div className='flex font-roboto items-center lg:h-[410px] h-[170px] justify-between w-full overflow-hidden'>
+           <div className='flex font-roboto items-center lg:h-[410px] h-[220px] justify-between w-full overflow-hidden'>
               <div className='flex flex-col lg:w-[445px]  w-[60%]'>
-                     <span className='text-[#34BCEC] lg:text-base whitespace-nowrap text-[12px] sm:font-semibold uppercase lg:pb-3 pb-1 tracking-widest anim-topTxt'>
+                     <span className='text-[#34BCEC] lg:text-base whitespace-nowrap text-[16px] sm:font-semibold uppercase lg:pb-3 pb-1 tracking-widest anim-topTxt'>
                       shop to get what you love
                  </span>
-                 <span className='uppercase w-42 lg:w-110 lg:text-[46px] text-[14px] font-thin text-[#333E48] anim-text lg:leading-14 '>timepieces that make a statement up to <span className='font-inter font-bold'>40% off</span></span>
+                 <span className='uppercase w-42 lg:w-110 lg:text-[46px] text-[14px] lg:font-thin text-[#333E48] anim-text lg:leading-14 leading-6'>timepieces that make a statement up to <span className='font-inter font-bold'>40% off</span></span>
                  <div className='lg:pt-9 pt-2 anim-btn'>
-                     <button className='relative group bg-primary lg:py-3 lg:px-14 px-4 py-1.5 lg:rounded-xl rounded-md tracking-wider lg:text-[16px] text-sm font-thin cursor-pointer overflow-hidden'>
+                     <button className='relative group bg-primary lg:py-3 lg:px-14 px-4 py-1.5 lg:rounded-xl rounded-md tracking-wider lg:text-[16px] text-sm lg:font-thin  cursor-pointer overflow-hidden'>
                         Start Buying
                     <span className='btnHover'/>
                      </button>
                  </div>
               </div >
               <div className='flex-1 flex justify-center  pt-12 lg:pt-0'>
-                <img src={SmartphonesSlider} className='slideImgTop lg:h-[790px] h-[400px] lg:pt-30 pl-1 lg:pl-0 w-auto object-contain' alt="SmartphonesSlider" />
+                <img src={SmartphonesSlider} className='slideImgTop lg:h-[790px] h-[400px] lg:pt-30 pt-12 pl-1 lg:pl-0 w-auto object-contain' alt="SmartphonesSlider" />
               </div>
            </div>
       </Container>
