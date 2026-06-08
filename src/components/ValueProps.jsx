f87deeddbc785e3c42da734b6868d1f13b74d8c6
Nurul Afsar Riyad <nurulafsarriyad06@gmail.com>
2026-06-08 14:56:12 +0600

    fix responsiveness

--- src/components/ValueProps.jsx
+++ src/components/ValueProps.jsx
@@ -9,25 +9,25 @@ const valueProps =  [
       icons: TruckElectric,
      },
      {
-      id: 1,
+      id: 2,
       title: '99% Positive',
       subTitle: 'Feedbacks',
       icons:  ThumbsUp,
      },
      {
-      id: 1,
+      id: 3,
       title: '365 days',
       subTitle: 'for free return',
       icons: RefreshCw,
      },
      {
-      id: 1,
+      id: 4,
       title: 'Payment',
       subTitle: 'Secure System',
       icons:  CreditCard,
      },
      {
-      id: 1,
+      id: 5,
       title: 'Only Best',
       subTitle: 'Brands',
       icons:  Tag,
@@ -37,15 +37,15 @@ const valueProps =  [
 let lastInd = valueProps.length - 1
 const ValueProps = () => {
   return (
-    <Container className='pt-12'>
-      <div className='border border-gray-300 rounded flex overflow-x-auto scroll-x-auto snap-x py-1 snap-mandatory touch-auto select-none'> 
+    <Container className='xl:pt-12 pt-8'>
+      <div className='border border-gray-300 rounded flex snap-center snap-x py-1 snap-mandatory touch-auto overflow-x-auto'> 
          {valueProps.map((item, id)=>{
          return(
-           <div key={id} className={`flex justify-center items-center gap-4 lg:py-5 py-2 px-7 lg:px-10 border-r whitespace-nowrap border-r-gray-200 ${id === lastInd && 'border-none'}`} >
+           <div key={id} className={`flex justify-center items-center gap-4 lg:py-3 py-2 px-7 lg:px-1 border-r whitespace-nowrap border-r-gray-200 ${id === lastInd && 'border-none'} flex-1`} >
             <item.icons className='text-primary' size={30} />
             <div className='text-center'>
-             <h3 className='font-inter font-bold text-[14px]'>{item.title}</h3>
-             <p className='font-inter text-[14px] text-gray-500'>{item.subTitle}</p>
+             <h3 className='font-inter font-bold text-[14px] dark:text-white'>{item.title}</h3>
+             <p className='font-inter text-[14px] text-gray-400'>{item.subTitle}</p>
             </div>
           </div>
          )
